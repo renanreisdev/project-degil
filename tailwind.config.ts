ef39e6a8dac4f79fa12b6932af40f029c3a9cac5
Renan Reis <renanreis.dev@gmail.com>
2023-10-11 20:12:36 -0300

    feat: Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,39 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundColor: {
+        primary: "#03466e",
+        secondary: "#ed631d",
+        hover: "#303f47"
+      },
+      textColor: {
+        pageTitle: "#03466e",
+        serviceTitle: "#f7bb5d",
+        secondary: "#ed631d",
+      },
+      borderColor: {
+        orange: "#ed631d",
       },
     },
+    screens: {
+      'xs': '544px',
+      // => @media (min-width: 544px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
