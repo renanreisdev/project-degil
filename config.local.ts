a6a7c2795c4f50a7c7e43f2f7689a4fc82006aff
Renan Reis <renanreis.dev@gmail.com>
2023-10-11 21:03:40 -0300

    provisional deploy

---
+++ config.local.ts
@@ -0,0 +1,9 @@
+export const config = {
+    PHONE: "51992646568", // celular whatsapp
+    INITIAL_M2: 60, // m² inicial
+    INITIAL_PRICE: 100, // Preço inicial
+    SURPLUS_M2: 1, // Valor do m² excedente
+    SEMI_FURNISHED: 0.20, // Valor adicional(%) Semi Mobiliado
+    FURNISHED: 0.50, // Valor adicional(%) Mobiliado
+    COUTYARD: 0.20 // Valor adicional(%) Pátio
+}
