198e69a3e750f9a9a6bf9a957365f4b93b40d649
Renan Reis <renanreis.dev@gmail.com>
2023-10-13 17:32:06 -0300

    refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Footer } from '@/components/Footer'
-import { NavContextProvider } from './context/navContext'
-
 
 const inter = Inter({ subsets: ['latin'] })
 
