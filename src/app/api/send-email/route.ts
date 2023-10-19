2f717aca2969e0bd3432edab4e1d914f201fea88
Renan Reis <renanreis.dev@gmail.com>
2023-10-19 10:26:26 -0300

    refactor: refactored the email sending API, with attachment validation

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,16 +1,52 @@
+import { NextRequest, NextResponse } from 'next/server'
 import nodemailer from 'nodemailer'
 
 type dataType = {
     subject?: string
-    name?: string,
-    email: string,
-    message?: string,
-    fileName?: string,
-    fileURL?: string
-
+    email?: string,
+    message?: string
+    carbonCopy?: boolean
 }
-export async function POST(request: Request) {
-    const data: dataType = await request.json()
+export async function POST(request: NextRequest) {
+    const formData = await request.formData()
+
+    let file: File | null = null
+    let buffer: Buffer | null = null
+
+    if (!formData.has("json") && !formData.has("file")) {
+        return NextResponse.json({
+            success: false,
+        })
+    } else if (formData.has("file")) {
+        file = formData.get("file") as unknown as File
+
+        const allowedExtensions = [".doc", ".docx", ".pdf"];
+
+        if (file.name.length > 0) {
+            const fileExtension = file.name.split('.').pop();
+
+            if (!allowedExtensions.includes(`.${fileExtension}`)) {
+                console.error("Escolha um arquivo do tipo .pdf, .doc ou .docx")
+                return NextResponse.json({
+                    success: false,
+                })
+            }
+
+            if (file.size > 25 * 1024 * 1024) {
+                console.error("O arquivo deve ter no máximo 25MB")
+                return NextResponse.json({
+                    success: false,
+                })
+            }
+        }
+    }
+
+    const { subject, email, message, carbonCopy }: dataType = JSON.parse(formData.get("json") as string)
+
+    if (file) {
+        const bytes = await file.arrayBuffer()
+        buffer = Buffer.from(bytes)
+    }
 
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
@@ -26,20 +62,21 @@ export async function POST(request: Request) {
         await transporter.sendMail({
             from: process.env.USERMAIL,
             to: process.env.USERMAIL,
-            replyTo: data.email,
-            subject: data.subject || `E-mail de contato: ${data.email}`,
-            html: `${data.message}`,
-            attachments: [
+            cc: carbonCopy ? email : '',
+            replyTo: email,
+            subject: subject || `E-mail de contato: ${email}`,
+            html: message,
+            attachments: file ? [
                 {
-                    filename: data.fileName,
-                    path: data.fileURL
+                    filename: file.name,
+                    content: buffer as Buffer,
                 }
-            ]
+            ] : [],
         })
 
-        return new Response(JSON.stringify({ message: "success" }))
+        return new Response(JSON.stringify({ success: true }))
     } catch (error) {
         console.error("Error sending email: ", error)
-        return new Response(JSON.stringify({ message: "error" }))
+        return new Response(JSON.stringify({ success: false }))
     }
 }
