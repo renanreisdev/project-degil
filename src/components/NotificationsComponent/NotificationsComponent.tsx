bddec93b2c1c5908cd3e1ff72551f20c4b67ce0e
Renan Reis <renanreis.dev@gmail.com>
2023-10-19 17:55:03 -0300

    style: improving notification disappearance

--- src/components/NotificationsComponent/NotificationsComponent.tsx
+++ src/components/NotificationsComponent/NotificationsComponent.tsx
@@ -11,11 +11,11 @@ type NotificationsComponentProps = HTMLAttributes<HTMLSpanElement> & {
 
 export const NotificationsComponent = ({ children, size, position, typeNotification, showNotification, className }: NotificationsComponentProps) => {
     const notificationSize = size === "sm" ? "min-w-[64px] px-4 py-2" : size === "md" ? "min-w-[128px] p-4" : "w-1/2 px-4 py-2"
-    const notificationPosition = position === "bottom-right" ? "fixed bottom-5 right-5" : position === "bottom-left" ? "fixed bottom-5 left-5" : ""
-    const notificationType = typeNotification === "warning" ? "bg-primary" : typeNotification === "success" ? "bg-green-500" : "bg-red-500"
+    const notificationPosition = position === "bottom-right" ? "bottom-5 right-5" : position === "bottom-left" ? "bottom-5 left-5" : ""
+    const notificationType = typeNotification === "success" ? "bg-green-500" : typeNotification === "error" ? "bg-red-500" : "bg-primary"
     return (
         <span
-            className={twMerge(`${showNotification ? notificationPosition : '-bottom-20'} z-30 flex justify-center items-center gap-3 ${notificationSize} text-white ${notificationType} rounded shadow-sm transition-all ease-in-out duration-1000`, className)}
+            className={twMerge(`fixed ${showNotification ? notificationPosition : position === 'bottom-right' ? '-bottom-20 right-5' : '-bottom-20 left-5'} z-30 flex justify-center items-center gap-3 ${notificationSize} text-white ${notificationType} rounded shadow-sm transition-all ease-in-out duration-1000`, className)}
         >
             {children}
         </span>
